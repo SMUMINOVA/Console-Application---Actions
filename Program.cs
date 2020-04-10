90034416f15ee0a3d1aea41ae5b11d3155a202b9
Sobira <sobira204@gmail.com>
2020-04-10 21:08:40 +0500

    create interface IPlay

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW10P._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
