d2d8fd5b11b28aac06c83fda5d8f6467caa956e2
Sobira <sobira204@gmail.com>
2020-04-10 21:09:27 +0500

    create interface IRecordable

--- Program.cs
+++ Program.cs
@@ -8,5 +8,29 @@ namespace HW10P._2
         {
             Console.WriteLine("Hello World!");
         }
+        interface IPlayable
+        {
+            public void Play() {
+                System.Console.WriteLine("I'm playing");
+            }
+            public void Pause(){
+                System.Console.WriteLine("I have paused");
+            }
+            public void Stop(){
+                System.Console.WriteLine("I have stopped");
+            }
+        }
+        interface IRecordable
+        {
+            public void Record() {
+                System.Console.WriteLine("I'm recording");
+            }
+            public void Pause(){
+                System.Console.WriteLine("I have paused");
+            }
+            public void Stop(){
+                System.Console.WriteLine("I have stopped");
+            }
+        }
     }
 }
